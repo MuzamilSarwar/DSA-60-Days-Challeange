2f863de8c45b9b8cfd6d884b156ab6206a8d8917
Muzamil(ThinkPad) <muzamilsarwar415@gmail.com>
2023-02-14 21:24:10 +0500

    First

---
+++ Day1/Find Char Index/FindIndex/FindIndex/Program.cs
@@ -0,0 +1,52 @@
+﻿//problem Statemnet
+
+    //find index of any character in a string. 
+
+    //example:
+
+    //find index 'c' in the word "Facebook"
+
+//Code For Main Function
+
+Console.WriteLine("Kindly Enter String");
+string str = Console.ReadLine();
+Console.WriteLine("Kindly Enter the Charecter Which you want To Find");
+char chr= char.Parse(Console.ReadLine());
+
+int result = FindIndexOfChar(str, chr);
+if( result>=0)
+{
+    Console.WriteLine($"index of c is {result}");
+}
+else
+{
+    Console.WriteLine("Given Char is not fond in String");
+}
+
+//Function for Finding Char
+int FindIndexOfChar(string s, char f)
+{
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (f == s[i])
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+//Function for finding char and its count
+
+void FindIndex(string s, char f)
+{
+    int count = 0;
+    for (int i = 0; i < s.Length; i++)
+    {
+        if (f == s[i])
+        {
+            count++;
+        }
+
+    }
+}
---
+++ Day1/PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,56 @@
+﻿//Problem Statement
+
+    //Find the Given Number is Prime or Not
+
+
+Console.WriteLine("kinldy enter any number");
+int num = int.Parse(Console.ReadLine());
+
+IsPrime(num);
+ISprime(num);
+void IsPrime(int num)
+{
+    int count = 0;
+
+    for (int i = 1; i <= num; i++)
+    {
+        if (num % 1 == 0 && num % i == 0)
+        {
+            count++;
+        }
+
+    }
+    if (count == 2)
+    {
+        Console.WriteLine("given number is prime ");
+    }
+    else
+    {
+        Console.WriteLine("Given number is not prime");
+    }
+}
+
+//Second Way
+
+void ISprime(int num)
+{
+    bool flag = false;
+
+    for (int i = 2; i <= num/2; i++)
+    {
+        if (num % i == 0)
+        {
+            flag = true;
+            break;
+        }
+
+    }
+    if (flag)
+    {
+        Console.WriteLine("given number is not prime ");
+    }
+    else
+    {
+        Console.WriteLine("Given number is prime");
+    }
+}
